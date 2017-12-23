c31fcd208898add9946519cede3c621fb0d1712d
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2017-12-22 22:31:54 -0200

    First commit
    Add some projects

---
+++ src/Consoles/AsyncTest001/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTest001
+{
+    class Program
+    {
+        static string f;
+        static string g;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            F();
+            G();
+            Console.WriteLine(f + g);
+
+            Console.ReadLine();
+        }
+
+        static async void F()
+        {
+            //Console.WriteLine("F start");
+            await Task.Delay(10);
+            f = "Hello";
+            //Console.WriteLine("F end");
+        }
+
+        static async void G()
+        {
+            //Console.WriteLine("G start");
+            Thread.Sleep(10);
+            g = " World!";
+            //Console.WriteLine("G end");
+        }
+    }
+}
---
+++ src/Consoles/DynamicType/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace .DynamicType
+{
+    class TextWriter
+    {
+        protected dynamic config;
+
+        public TextWriter(dynamic config)
+        {
+            this.config = config;
+        }
+
+        public void getFileName()
+        {
+            Console.WriteLine(config.FileName + config.Extension);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new { Extension = ".txt", FileName = "poem" };
+            TextWriter txt = new TextWriter(config);
+            txt.getFileName();
+
+            Console.ReadLine();
+        }
+    }
+}

55dc3867a80a00a8d93bfc3a7c8301d39ddf008e
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2022-03-15 00:49:55 -0300

    Cria controller de experimento de tipos de retorno

---
+++ src/Lab.Api/Controllers/TiposRetornoActionController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Api.Controllers
+{
+    public record MinhaClasse(string Nome);
+
+    public class TiposRetornoActionController : Controller
+    {
+        public string RetornoTipoPrimitivo()
+        {
+            if (DateTime.Now == new DateTime(2022, 1, 20))
+            {
+                //return BadRequest(); // -> Erro CS0029 Cannot implicitly convert type 'Microsoft.AspNetCore.Mvc.BadRequestResult' to 'string'
+                return "badrequest";
+            }
+
+            return DateTime.Now.ToString();
+        }
+        public MinhaClasse RetornoTipoComplexo()
+        {
+            if (DateTime.Now == new DateTime(2022, 1, 20))
+            {
+                //return BadRequest(); // -> Erro CS0029  Cannot implicitly convert type 'Microsoft.AspNetCore.Mvc.BadRequestResult' to 'Lab.Api.Controllers.MinhaClasse'
+                return new("badrequest");
+            }
+
+            return new("Pedro");
+        }
+        public ActionResult<string> RetornoActionResultTipoPrimitivo()
+        {
+            if (DateTime.Now == new DateTime(2022, 1, 20))
+                return BadRequest();
+
+            return DateTime.Now.ToString();
+        }
+        public ActionResult<MinhaClasse> RetornoActionResultTipoComplexo()
+        {
+            if (DateTime.Now == new DateTime(2022, 1, 20))
+                return BadRequest();
+
+            var retorno = new MinhaClasse("Pedro");
+            return Ok(retorno);
+        }
+    }
+}

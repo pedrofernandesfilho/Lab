f46d1b43f264558bcf928815c7a79a8c59aa3c54
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2024-07-07 16:44:22 -0300

    Adiciona ApiController em TiposRetorno Controller

--- src/Lab.Api/Controllers/TiposRetornoActionController.cs
+++ src/Lab.Api/Controllers/TiposRetornoActionController.cs
@@ -1,45 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Lab.Api.Controllers
+namespace Lab.Api.Controllers;
+
+public record MinhaClasse(string Nome);
+
+[ApiController]
+[Route("[controller]")]
+public class TiposRetornoActionController : Controller
 {
-    public record MinhaClasse(string Nome);
-
-    public class TiposRetornoActionController : Controller
+    [HttpGet("tipo-primitivo")]
+    public string RetornoTipoPrimitivo()
     {
-        public string RetornoTipoPrimitivo()
+        if (DateTime.Now == new DateTime(2022, 1, 20))
         {
-            if (DateTime.Now == new DateTime(2022, 1, 20))
-            {
-                //return BadRequest(); // -> Erro CS0029 Cannot implicitly convert type 'Microsoft.AspNetCore.Mvc.BadRequestResult' to 'string'
-                return "badrequest";
-            }
-
-            return DateTime.Now.ToString();
+            //return BadRequest(); // -> Erro CS0029 Cannot implicitly convert type 'Microsoft.AspNetCore.Mvc.BadRequestResult' to 'string'
+            return "badrequest";
         }
-        public MinhaClasse RetornoTipoComplexo()
+
+        return DateTime.Now.ToString();
+    }
+
+    [HttpGet("tipo-complexo")]
+    public MinhaClasse RetornoTipoComplexo()
+    {
+        if (DateTime.Now == new DateTime(2022, 1, 20))
         {
-            if (DateTime.Now == new DateTime(2022, 1, 20))
-            {
-                //return BadRequest(); // -> Erro CS0029  Cannot implicitly convert type 'Microsoft.AspNetCore.Mvc.BadRequestResult' to 'Lab.Api.Controllers.MinhaClasse'
-                return new("badrequest");
-            }
-
-            return new("Pedro");
+            //return BadRequest(); // -> Erro CS0029  Cannot implicitly convert type 'Microsoft.AspNetCore.Mvc.BadRequestResult' to 'Lab.Api.Controllers.MinhaClasse'
+            return new("badrequest");
         }
-        public ActionResult<string> RetornoActionResultTipoPrimitivo()
-        {
-            if (DateTime.Now == new DateTime(2022, 1, 20))
-                return BadRequest();
 
-            return DateTime.Now.ToString();
-        }
-        public ActionResult<MinhaClasse> RetornoActionResultTipoComplexo()
-        {
-            if (DateTime.Now == new DateTime(2022, 1, 20))
-                return BadRequest();
+        return new("Pedro");
+    }
 
-            var retorno = new MinhaClasse("Pedro");
-            return Ok(retorno);
-        }
+    [HttpGet("tipo-primitivo-action-result")]
+    public ActionResult<string> RetornoActionResultTipoPrimitivo()
+    {
+        if (DateTime.Now == new DateTime(2022, 1, 20))
+            return BadRequest();
+
+        return DateTime.Now.ToString();
+    }
+
+    [HttpGet("tipo-complexo-action-result")]
+    public ActionResult<MinhaClasse> RetornoActionResultTipoComplexo()
+    {
+        if (DateTime.Now == new DateTime(2022, 1, 20))
+            return BadRequest();
+
+        var retorno = new MinhaClasse("Pedro");
+        return Ok(retorno);
     }
 }
